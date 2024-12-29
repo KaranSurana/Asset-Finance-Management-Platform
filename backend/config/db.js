ce3e0fbb6e74037d209100cf788df6893d9bce55
Karan Surana <karansurana23@gmail.com>
2024-12-29 23:55:06 +1100

    Feature Bonus 3/4: Prod Fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
     retryReads: true               
   };
   try {
-    await mongoose.connect(process.env.MONGODB_URI,options);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
