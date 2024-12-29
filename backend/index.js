0f705690e85e94266a16c4c7cf35fb3b35350f76
Karan Surana <karansurana23@gmail.com>
2024-12-30 00:51:49 +1100

    Feature Bonus 5: Configured Swagger

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
-require('dotenv').config();
-
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 const userRoutes = require('./routes/userRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
-
 const app = express();
+
+require('dotenv').config();
 
 app.use(express.json());
 app.use(cors());
@@ -15,13 +18,17 @@ const connectDB = require("./config/db");
 connectDB();
 
 app.get('/health', (req,res)=>{
-    res.send("Wo  in ");
+    res.send("Working");
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('/user', userRoutes);
+
 app.use('/applications', applicationRoutes);
 
 // const PORT = process.env.PORT || 5000;
-// app.listen(PORT, '0.0.0.0', () => {
+// app.listen(PORT,  () => {
 //     console.log(`Server is running on port ${PORT}`);
 // });
 
